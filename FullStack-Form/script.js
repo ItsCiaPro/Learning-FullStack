a563ebbd42572fb7b89934d11dcd0a6f5c15c8e4
ItsCiaPro <josuemedrado193@gmail.com>
2026-07-21 22:05:39 -0300

    finish login & registration screen ui and animations

---
+++ FullStack-Form/script.js
@@ -0,0 +1,17 @@
+let formsContainer = document.getElementById('form-box')
+
+function switchForm(formId) {
+   document.getElementById('login-form').classList.remove('active');
+   document.getElementById('register-form').classList.remove('active');
+
+   document.getElementById(formId).classList.add('active')
+
+   if (formsContainer.classList.contains('slide-left')) {
+      formsContainer.classList.remove('slide-left');
+      formsContainer.classList.add('slide-right')
+
+   } else if (formsContainer.classList.contains('slide-right')) {
+      formsContainer.classList.remove('slide-right');
+      formsContainer.classList.add('slide-left')
+   } 
+}
